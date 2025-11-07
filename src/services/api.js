48431ddcdfc7a16f682a3e963f234f9b846906ff
DinhAnnh <96048192+DinhAnnh@users.noreply.github.com>
2025-11-08 00:34:16 +0700

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.DEV ? '/api' : 'http://localhost:5001/api',
+  baseURL: import.meta.env.DEV ? '/api' : 'https://nhatro.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
